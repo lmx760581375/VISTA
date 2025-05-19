1ab91f736c12fe7d997e502218ddedcc77891f08
mingxiaoli <760581375@qq.com>
2025-05-19 20:12:51 +0800

    VISTA release update

--- llava/train/train_mem.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.getcwd())
-print(sys.path)
-from llava.train.train import train
-
-if __name__ == "__main__":
-    train(attn_implementation="flash_attention_2")
